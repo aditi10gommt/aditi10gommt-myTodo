0fd33934384a7ad8c9fa34b940e8ae9a390e3060
aditi10gommt <61961893+aditi10gommt@users.noreply.github.com>
2020-03-28 12:28:20 +0530

    Add files via upload
    myTodo application -code at the time of first review

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# aditi10gommt-myTodo
-Todo list application built using JS,HTML,CSS
+================ myTodo Introduction ============
+
+The project covers complete frontend side of todo application.
+Built using HTML, JavaScript , CSS.
+
+A todo card can be added to the application along with the title of the list.
+
+Once task is completed mark the task as done and checkbox will get disabled.
+
+Also, once all the tasks under a title are completed you can delete the todo card with 'x' button provided.
+
+Data is made persistant using localstorage.
+
+
+
+
+
+
---
+++ myTodo.js
@@ -0,0 +1,329 @@
+let cardId = 0;
+
+window.onload = init;
+
+function init() {
+  let localStorageData = getDataFromLocalStorage();
+  if (!localStorageData || localStorageData.length == 0) {
+    document.getElementById("emptyBoard").innerHTML =
+      "Add your first task here!";
+    return;
+  }
+  for (let index = 0; index < localStorageData.length; index++) {
+    const taskTitle = localStorageData[index].title;
+    const creationTime = localStorageData[index].creationTime;
+    let taskItems = localStorageData[index].tasks;
+    createCard(taskItems, taskTitle, creationTime);
+  }
+}
+
+function createCard(taskItems, taskTitle, creationTime) {
+  let chars = 0;
+  chars = chars + taskTitle.length;
+  let cardSize = 1;
+  let k = 0;
+
+  let newCard = {
+    title: taskTitle,
+    creationTime: creationTime,
+    tasks: []
+  };
+  let short_title = taskTitle;
+  if (chars > 70) {
+    short_title = taskTitle.slice(0, 70);
+    short_title = short_title + "...";
+  }
+
+  taskItems.sort(function(x, y) {
+    return x.done === y.done ? 0 : x.done ? 1 : -1;
+  });
+
+  const columnDiv = document.createElement("div");
+  columnDiv.setAttribute("class", "column");
+  columnDiv.setAttribute("id", `column${cardId}`);
+
+  const closeDiv = document.createElement("span");
+  closeDiv.setAttribute("class", "close");
+  const closeX = document.createTextNode("x");
+  closeDiv.appendChild(closeX);
+  closeDiv.setAttribute("onclick", `removeCard(${cardId})`);
+
+  const zoomDiv = document.createElement("span");
+  zoomDiv.setAttribute("id", "btnExp");
+  const zoomX = document.createTextNode("View");
+  zoomDiv.appendChild(zoomX);
+  zoomDiv.setAttribute("onclick", `expandCard(${cardId})`);
+
+  const cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card");
+
+  const titleDiv = document.createElement("div");
+  titleDiv.setAttribute("class", "title");
+  const newTitle = document.createTextNode(short_title);
+  titleDiv.appendChild(newTitle);
+
+  const ulDiv = document.createElement("ul");
+  ulDiv.setAttribute("id", "list");
+  taskItems.forEach(element => {
+    //for each starts here
+    const newTodo = document.createElement("li");
+    let addTask = document.createTextNode(element.name);
+    newTodo.setAttribute("id", "lis");
+
+    newCard.tasks.push(element);
+    chars = chars + element.name.length;
+    if (cardSize < 4 && chars < 50) {
+      newTodo.appendChild(addTask);
+      ulDiv.appendChild(newTodo);
+      cardSize++;
+    } else if (cardSize < 4 && chars > 50 && chars < 70) {
+      let toBeContinued = element.name.slice(0, 30);
+      toBeContinued = toBeContinued + "...";
+      let addTask = document.createTextNode(toBeContinued);
+      newTodo.appendChild(addTask);
+      ulDiv.appendChild(newTodo);
+      cardSize++;
+    }
+    k++;
+  }); // end of forEach loop
+  if (taskItems.length > 3) {
+    const largeCardIndicator = document.createElement("span");
+    const enlarger = document.createTextNode(" . . . ");
+    largeCardIndicator.appendChild(enlarger);
+    zoomDiv.appendChild(largeCardIndicator);
+  }
+
+  cardDiv.appendChild(titleDiv);
+  cardDiv.appendChild(ulDiv);
+  columnDiv.appendChild(closeDiv);
+  columnDiv.appendChild(zoomDiv);
+  columnDiv.appendChild(cardDiv);
+  document.querySelector(".row").appendChild(columnDiv);
+
+  document.getElementById("titleInput").value = "";
+  document.querySelector("#listInput").innerHTML = "";
+  cardId++;
+  document.getElementById("emptyBoard").innerHTML = " ";
+
+  return newCard;
+}
+
+//when submit button gets clicked
+function addTask() {
+  let taskTitle = document.getElementById("titleInput").value;
+
+  const dateTime = getDateTime();
+  if (taskTitle == "") {
+    taskTitle = "My Todo";
+  }
+  if (taskItems.length == 0) {
+    document.getElementById("emptyListErr").innerHTML = "Please add tasks!";
+  } else {
+    document.getElementById("emptyListErr").innerHTML = "";
+    let newCard = createCard(taskItems, taskTitle, dateTime);
+    pushCardInLocalstorage(newCard);
+    span.onclick();
+    del();
+  }
+}
+
+let taskItems = [];
+//adding to modal list
+function addToList() {
+  let toDo = document.querySelector("#input").value;
+  if (toDo == "") {
+    document.getElementById("emptyTaskErr").innerHTML =
+      "Please enter a valid task !";
+  } else {
+    document.getElementById("emptyTaskErr").innerHTML = "";
+    document.getElementById("emptyListErr").innerHTML = "";
+
+    let createTodo = document.createElement("li");
+    createTodo.setAttribute("id", "newTask");
+    let addTodo = document.createTextNode(toDo);
+    createTodo.appendChild(addTodo);
+    document.querySelector("#listInput").appendChild(createTodo);
+    document.querySelector("#input").value = "";
+    let newTask = {
+      name: toDo,
+      done: false
+    };
+    taskItems.push(newTask);
+  }
+}
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("closing")[0];
+
+function openAddTaskModal() {
+  taskItems = [];
+  document.getElementById("overlay").style = "display:block";
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+  document.getElementById("overlay").style = "display:none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var closeExp = document.getElementsByClassName("closeExp")[0];
+closeExp.onclick = function() {
+  location.reload();
+  document.querySelector(".exp").innerHTML = "";
+  myModalExp.style.display = "none";
+  document.getElementById("overlay").style = "display:none";
+};
+
+function expandCard(cardNumber) {
+  document.querySelector(".dateTimeExp").innerHTML = "";
+  document.getElementById("overlay").style = "display:block";
+  document.getElementById("myModalExp").style.display = "block";
+
+  const localStorageData = getDataFromLocalStorage();
+  let k = 0;
+
+  for (let index = 0; index < localStorageData.length; index++) {
+    if (cardNumber == index) {
+      var taskTitle = localStorageData[index].title;
+      var taskItems = localStorageData[index].tasks;
+      var creationTime = localStorageData[index].creationTime;
+      break;
+    }
+  }
+
+  const columnDiv = document.createElement("div");
+  columnDiv.setAttribute("class", "column");
+  columnDiv.setAttribute("id", `column${cardNumber}`);
+
+  const cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "cardModal");
+
+  const titleDiv = document.createElement("div");
+  titleDiv.setAttribute("class", "title");
+  const newTitle = document.createTextNode(taskTitle);
+  titleDiv.appendChild(newTitle);
+
+  const ulDiv = document.createElement("ul");
+  ulDiv.setAttribute("id", "list");
+
+  let displayDateTime = document.createElement("div");
+  displayDateTime.innerHTML = creationTime;
+
+  taskItems.forEach(element => {
+    //for each starts here
+    const newTodo = document.createElement("li");
+    const addTask = document.createTextNode(element.name);
+    newTodo.setAttribute("id", "lis");
+
+    const chk = document.createElement("input");
+    chk.setAttribute("type", "checkbox");
+    chk.setAttribute("id", "myCheck");
+    const chkId = `column${cardNumber}${k++}`;
+    chk.setAttribute("class", `${chkId}`);
+    newTodo.setAttribute("class", `${chkId}`);
+    chk.setAttribute("onclick", `changeStatusAsDone(${cardNumber},${k - 1})`);
+
+    newTodo.appendChild(chk);
+    newTodo.appendChild(addTask);
+    ulDiv.appendChild(newTodo);
+  }); // end of forEach loop
+
+  cardDiv.appendChild(titleDiv);
+  cardDiv.appendChild(ulDiv);
+  columnDiv.appendChild(cardDiv);
+  document.querySelector(".exp").appendChild(columnDiv);
+  document.querySelector(".dateTimeExp").appendChild(displayDateTime);
+
+  document.getElementById("titleInput").value = "";
+  document.querySelector("#listInput").innerHTML = "";
+  markDone(cardNumber);
+}
+
+function removeCard(cardNumber) {
+  const remCol = `column${cardNumber}`;
+  const element = document.getElementById(remCol);
+  let localStorageData = getDataFromLocalStorage();
+  element.remove();
+  for (let i = 0; i < localStorageData.length; i++) {
+    if (i == cardNumber) {
+      localStorageData.splice(i, 1);
+      break;
+    }
+  }
+  setDataInLocalStorage(localStorageData);
+  location.reload();
+}
+
+function getDateTime() {
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let dateTime = date + "\xa0\xa0\xa0\xa0\xa0" + time;
+  return dateTime;
+}
+
+function getDataFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("todoList"));
+}
+
+function pushCardInLocalstorage(localStorageData) {
+  let oldData = JSON.parse(localStorage.getItem("todoList")) || [];
+  oldData.push(localStorageData);
+  localStorageData = JSON.stringify(oldData);
+  localStorage.setItem("todoList", localStorageData);
+
+  return;
+}
+
+function setDataInLocalStorage(localStorageData) {
+  localStorageData = JSON.stringify(localStorageData);
+  localStorage.setItem("todoList", localStorageData);
+}
+
+const SPAN_ELEMENT_ITEM_TITLE = 0;
+const INPUT_ELEMENT_CHECKBOX = 1;
+
+function strikeThroughItem(cardNumber, itemNumber) {
+  const refernce = `column${cardNumber}${itemNumber}`;
+  const listItem = document.querySelectorAll(`.${refernce}`);
+  listItem[SPAN_ELEMENT_ITEM_TITLE].style.textDecoration = "line-through";
+  listItem[INPUT_ELEMENT_CHECKBOX].disabled = true;
+  listItem[INPUT_ELEMENT_CHECKBOX].checked = true;
+}
+
+function changeStatusAsDone(cardNumber, taskNumber) {
+  let localStorageData = getDataFromLocalStorage();
+  localStorageData[cardNumber].tasks[taskNumber].done = true;
+  setDataInLocalStorage(localStorageData);
+  strikeThroughItem(cardNumber, taskNumber);
+}
+
+function markDone(cardNumber) {
+  let allTasksDone = 0;
+  const localStorageData = getDataFromLocalStorage();
+  const currentTask = localStorageData[cardNumber];
+  const taskItemsLength = currentTask.tasks.length;
+
+  for (let j = 0; j < taskItemsLength; j++) {
+    if (currentTask.tasks[j].done) {
+      strikeThroughItem(cardNumber, j);
+      allTasksDone++;
+    }
+  }
+  if (allTasksDone == taskItemsLength) {
+    let askToRemove = window.confirm(
+      "All tasks are done in the list. Would you like to delete it?"
+    );
+    if (askToRemove) {
+      removeCard(cardNumber);
+    }
+  }
+}
